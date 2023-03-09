88f9dd124ea74f5b93eacefdd6237cda26938aa6
Dominick Agnello <dagnello@asu.edu>
2023-03-09 02:21:51 -0700

    get up to date with master

--- src/hooks/use-site-metadata.tsx
+++ src/hooks/use-site-metadata.tsx
@@ -1,4 +1,4 @@
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from 'gatsby';
 
 export const useSiteMetadata = () => {
   const data = useStaticQuery(graphql`
@@ -10,7 +10,7 @@ export const useSiteMetadata = () => {
         }
       }
     }
-  `)
+  `);
 
-  return data.site.siteMetadata
-}
+  return data.site.siteMetadata;
+};
